5654b36bc7cbcade500645c0eba20e659eb4ad6d
Michael Ganss <michael@ganss.org>
2014-10-08 21:18:19 +0200

    Make mouse behavior more like standard

--- MainForm.cs
+++ MainForm.cs
@@ -18,21 +18,21 @@ namespace BorderlessForm
         {
             InitializeComponent();
 
-            TopLeftCornerPanel.MouseDown += (s, e) => DecorationMouseDown(HitTestValues.HTTOPLEFT);
-            TopRightCornerPanel.MouseDown += (s, e) => DecorationMouseDown(HitTestValues.HTTOPRIGHT);
-            BottomLeftCornerPanel.MouseDown += (s, e) => DecorationMouseDown(HitTestValues.HTBOTTOMLEFT);
-            BottomRightCornerPanel.MouseDown += (s, e) => DecorationMouseDown(HitTestValues.HTBOTTOMRIGHT);
+            TopLeftCornerPanel.MouseDown += (s, e) => DecorationMouseDown(e, HitTestValues.HTTOPLEFT);
+            TopRightCornerPanel.MouseDown += (s, e) => DecorationMouseDown(e, HitTestValues.HTTOPRIGHT);
+            BottomLeftCornerPanel.MouseDown += (s, e) => DecorationMouseDown(e, HitTestValues.HTBOTTOMLEFT);
+            BottomRightCornerPanel.MouseDown += (s, e) => DecorationMouseDown(e, HitTestValues.HTBOTTOMRIGHT);
 
-            TopBorderPanel.MouseDown += (s, e) => DecorationMouseDown(HitTestValues.HTTOP);
-            LeftBorderPanel.MouseDown += (s, e) => DecorationMouseDown(HitTestValues.HTLEFT);
-            RightBorderPanel.MouseDown += (s, e) => DecorationMouseDown(HitTestValues.HTRIGHT);
-            BottomBorderPanel.MouseDown += (s, e) => DecorationMouseDown(HitTestValues.HTBOTTOM);
+            TopBorderPanel.MouseDown += (s, e) => DecorationMouseDown(e, HitTestValues.HTTOP);
+            LeftBorderPanel.MouseDown += (s, e) => DecorationMouseDown(e, HitTestValues.HTLEFT);
+            RightBorderPanel.MouseDown += (s, e) => DecorationMouseDown(e, HitTestValues.HTRIGHT);
+            BottomBorderPanel.MouseDown += (s, e) => DecorationMouseDown(e, HitTestValues.HTBOTTOM);
 
-            SystemLabel.Click += (s, e) => ShowSystemMenu(MouseButtons, PointToScreen(new Point(8, 32)));
-            SystemLabel.DoubleClick += (s, e) => Close();
+            SystemLabel.MouseDown += SystemLabel_MouseDown;
+            SystemLabel.MouseUp += SystemLabel_MouseUp;
 
             TitleLabel.MouseDown += TitleLabel_MouseDown;
-            TitleLabel.MouseUp += (s, e) => { if (e.Button == MouseButtons.Right) ShowSystemMenu(MouseButtons); };
+            TitleLabel.MouseUp += (s, e) => { if (e.Button == MouseButtons.Right && TitleLabel.ClientRectangle.Contains(e.Location)) ShowSystemMenu(MouseButtons); };
             TitleLabel.Text = Text;
             TextChanged += (s, e) => TitleLabel.Text = Text;
 
@@ -43,11 +43,48 @@ namespace BorderlessForm
             CloseLabel.Font = marlett;
             SystemLabel.Font = marlett;
 
-            MinimizeLabel.Click += (s, e) => WindowState = FormWindowState.Minimized;
-            MaximizeLabel.Click += (s, e) => ToggleMaximize();
+            MinimizeLabel.MouseClick += (s, e) => { if (e.Button == MouseButtons.Left) WindowState = FormWindowState.Minimized; };
+            MaximizeLabel.MouseClick += (s, e) => { if (e.Button == MouseButtons.Left) ToggleMaximize(); };
             previousWindowState = MinMaxState;
             SizeChanged += MainForm_SizeChanged;
-            CloseLabel.Click += (s, e) => Close();
+            CloseLabel.MouseClick += (s, e) => Close(e);
+        }
+
+        void SystemLabel_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right) ShowSystemMenu(MouseButtons);
+        }
+
+        private DateTime systemClickTime = DateTime.MinValue;
+        private DateTime systemMenuCloseTime = DateTime.MinValue;
+
+        void SystemLabel_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                var clickTime = (DateTime.Now - systemClickTime).TotalMilliseconds;
+                if (clickTime < SystemInformation.DoubleClickTime)
+                    Close();
+                else
+                {
+                    systemClickTime = DateTime.Now;
+                    if ((systemClickTime - systemMenuCloseTime).TotalMilliseconds > 200)
+                    {
+                        ShowSystemMenu(MouseButtons, PointToScreen(new Point(8, 32)));
+                        systemMenuCloseTime = DateTime.Now;
+                    }
+                }
+            }
+        }
+
+        void Close(MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left) Close();
+        }
+
+        void DecorationMouseDown(MouseEventArgs e, HitTestValues h)
+        {
+            if (e.Button == MouseButtons.Left) DecorationMouseDown(h);
         }
 
         void MainForm_SizeChanged(object sender, EventArgs e)
@@ -79,7 +116,7 @@ namespace BorderlessForm
                     TitleLabel.Width += LeftBorderPanel.Width + RightBorderPanel.Width;
                     TitleLabel.Top = 0;
                 }
-                else
+                else if (previousWindowState == FormWindowState.Maximized)
                 {
                     SystemLabel.Left = LeftBorderPanel.Width;
                     SystemLabel.Top = TopBorderPanel.Height;
@@ -98,8 +135,6 @@ namespace BorderlessForm
             }
         }
 
-        private DateTime systemClickTime = DateTime.MinValue;
-
         private FormWindowState ToggleMaximize()
         {
             return WindowState = WindowState == FormWindowState.Maximized ? FormWindowState.Normal : FormWindowState.Maximized;
@@ -109,13 +144,16 @@ namespace BorderlessForm
 
         void TitleLabel_MouseDown(object sender, MouseEventArgs e)
         {
-            var clickTime = (DateTime.Now - titleClickTime).TotalMilliseconds;
-            if (clickTime < SystemInformation.DoubleClickTime)
-                ToggleMaximize();
-            else
+            if (e.Button == MouseButtons.Left)
             {
-                titleClickTime = DateTime.Now;
-                DecorationMouseDown(HitTestValues.HTCAPTION);
+                var clickTime = (DateTime.Now - titleClickTime).TotalMilliseconds;
+                if (clickTime < SystemInformation.DoubleClickTime)
+                    ToggleMaximize();
+                else
+                {
+                    titleClickTime = DateTime.Now;
+                    DecorationMouseDown(HitTestValues.HTCAPTION);
+                }
             }
         }
     }
